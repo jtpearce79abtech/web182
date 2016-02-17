4d7965c7e75fcf447821193dbd042899b9f71922
Jared Pearce <jaredtpearce@students.abtech.edu>
2016-02-17 10:32:18 -0500

    Completed Exercise Parts 3-6

--- ex_starts/ch08_ex1a/index.php
+++ ex_starts/ch08_ex1a/index.php
@@ -3,22 +3,27 @@
 $customer_type = filter_input(INPUT_POST, 'type');
 $invoice_subtotal = filter_input(INPUT_POST, 'subtotal');
 
-if ($customer_type == 'R') {
+if ($customer_type == 'R' || $customer_type == 'r' ) {
     if ($invoice_subtotal < 100) {
         $discount_percent = .0;
     } else if ($invoice_subtotal >= 100 && $invoice_subtotal < 250) {
         $discount_percent = .1;
-    } else if ($invoice_subtotal >= 250) {
+    } else if ($invoice_subtotal >= 250 && $invoice_subtotal < 500) {
         $discount_percent = .25;
+	}
+	  else if ($invoice_subtotal >= 500) {
+		$discount_percent = .3;
     }
-} else if ($customer_type == 'C') {
-    if ($invoice_subtotal < 250) {
+} else if ($customer_type == 'C' || $customer_type == 'c') {
         $discount_percent = .2;
-    } else {
-        $discount_percent = .3;
+} else if ($customer_type == 'T' || $customer_type == 't') {
+    if ($invoice_subtotal < 500) {
+        $discount_percent = .4;
+    } else if ($invoice_subtotal >= 500) {
+        $discount_percent = .5;
     }
 } else {
-    $discount_percent = .0;
+    $discount_percent = .1;
 }
 
 $discount_amount = $invoice_subtotal * $discount_percent;
