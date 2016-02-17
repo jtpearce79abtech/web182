e9ca687fedbc38f511063a66b5245c03902cb6e2
Jared Pearce <jaredtpearce@students.abtech.edu>
2016-02-17 10:44:48 -0500

    Completed Chapter 8-1 Parts 7-9

--- ex_starts/ch08_ex1b/index.php
+++ ex_starts/ch08_ex1b/index.php
@@ -3,6 +3,60 @@
 $customer_type = filter_input(INPUT_POST, 'type');
 $invoice_subtotal = filter_input(INPUT_POST, 'subtotal');
 
+$customer_type = strtoupper($customer_type);
+
+switch ($customer_type)
+{
+    case 'R':
+        if ($invoice_subtotal < 100)
+        {
+            $discount_percent = .0;
+        } 
+        else if ($invoice_subtotal >= 100 && $invoice_subtotal < 250)
+        {
+            $discount_percent = .1;
+        }
+        else if ($invoice_subtotal >= 250)
+        {
+            $discount_percent = .25;
+        }
+    break;
+    
+    case 'C':
+        if ($customer_type == 'C')
+        {
+            if ($invoice_subtotal < 250)
+            {
+                $discount_percent = .2;
+            }
+            else
+            {
+                $discount_percent = .3;
+            }
+        }
+    break;
+        
+    case 'T':
+        if ($customer_type == 'T')
+        {
+            if ($invoice_subtotal >= 500)
+            {
+                $discount_percent = .5;
+            }
+            else
+            {
+                $discount_percent = .4;
+            }
+        }
+    break;
+        
+    default:
+        {
+            $discount_percent = .1;
+        }
+}
+
+/*
 if ($customer_type == 'R') {
     if ($invoice_subtotal < 100) {
         $discount_percent = .0;
@@ -20,6 +74,7 @@ if ($customer_type == 'R') {
 } else {
     $discount_percent = .0;
 }
+*/
 
 $discount_amount = $invoice_subtotal * $discount_percent;
 $invoice_total = $invoice_subtotal - $discount_amount;
